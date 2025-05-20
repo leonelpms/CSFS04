2737ef5e7f0a997776bb3148eb07def03abcfb53
leonelpms <leonel.ibarra@people-media.com.mx>
2025-05-20 12:07:38 -0600

    Add project files.

---
+++ Shared/EventModel.cs
@@ -0,0 +1,10 @@
+namespace CS_FS04.Shared
+{
+    public class EventModel
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public DateTime Date { get; set; }
+        public string Location { get; set; }
+    }
+}
